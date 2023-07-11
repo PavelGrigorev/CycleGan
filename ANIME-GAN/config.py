5635f67fbb2b0996ba607b2f798d26903fc654c0
PavelGrigorev <66916753+PavelGrigorev@users.noreply.github.com>
2023-07-11 12:40:31 +0300

    Update config.py
    Change number epochs to 200, and log period to 10.

--- ANIME-GAN/config.py
+++ ANIME-GAN/config.py
@@ -1,12 +1,12 @@
 import argparse
 
-parser = argparse.ArgumentParser("DCGAN")
+parser = argparse.ArgumentParser("CycleGAN")
 parser.add_argument('--anime_dir', type=str, default="anime/images") # anime dataset directory
 parser.add_argument('--faces_dir', type=str, default="faces_dataset/faces_dataset/faces_dataset_small") # faces dataset directory
 parser.add_argument('--result_dir', type=str, default="./result_sample") # log image directory
 parser.add_argument('--batch_size', type=int, default=1) # batch size
-parser.add_argument('--n_epoch', type=int, default=2) # epoch size
-parser.add_argument('--log_iter', type=int, default=1) # print log message and save image per log_iter
+parser.add_argument('--n_epoch', type=int, default=200) # epoch size
+parser.add_argument('--log_iter', type=int, default=10) # print log message and save image per log_iter
 parser.add_argument('--anime_size', type=int, default=300)  # noise dimension
 parser.add_argument('--nc', type=int, default=3)    # input and out channel
 parser.add_argument('--lr', type=float, default=0.01) # learning rate
